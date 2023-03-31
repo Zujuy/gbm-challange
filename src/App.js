ea3712da4b04b7983bd3b5071ce316146ebad0e8
Zujuy <itza_zujuy@hotmail.co.uk>
2023-03-31 00:16:49 -0600

    adding functions with request to the service and charts package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+function IPCGraph() {
+  const [ipcData, setIPCData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get('https://run.mocky.io/v3/cc4c350b-1f11-42a0-a1aa-f8593eafeb1e');
+      setIPCData(response.data);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <LineChart width={800} height={400} data={ipcData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="Date" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="Price" stroke="#8884d8" />
+    </LineChart>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>IPC Indicator History</h1>
+      <IPCGraph />
     </div>
   );
 }
