08768a830bee8e8f5e900f7f10f3a81598c0431e
Zujuy <itza_zujuy@hotmail.co.uk>
2023-04-04 19:01:33 -0600

    restore logout file

---
+++ src/components/Logout.js
@@ -0,0 +1,30 @@
+import { GoogleLogout } from 'react-google-login';
+import {ButtonStyles, GoogleButonStyles } from '../styles/LogoutStyles';
+
+const client_Id = "619197449057-db5nnko9qdeb8v0b54u7muv6mppasb8j.apps.googleusercontent.com";
+
+function Logout({handleLogin}) {
+
+    const onLogoutSuccess = () => {
+        return(
+            ('Login out ok!'),
+            handleLogin(false)
+        )
+    };
+
+    console.log("here",onLogoutSuccess)
+
+    return(
+        <ButtonStyles>
+            <GoogleButonStyles>
+            <GoogleLogout 
+                clientId={client_Id}
+                buttonText="Logout"
+                onLogoutSuccess={onLogoutSuccess}
+            />
+            </GoogleButonStyles>
+        </ButtonStyles>
+    )
+};
+
+export default Logout
